ddcdafdaf31e357f6bd0a31a77abdb543ad85639
KP717 <kumar7777.kp@gmail.com>
2022-07-15 23:04:48 +0530

    second commit

--- dd.java
+++ dd.java
@@ -1,3 +1,12 @@
 public class dd {
     
+    
+    public static void main(String[] args) {
+
+        int a =46;
+        int b = 64;
+        int c = a+b;
+        System.out.println("The sum of two numb is "+c);
+        
+    }
 }
